e11992618808d02b4e7f367a1c43a1a7f394e90d
BabluSingh7 <bablusinghkb1@gmail.com>
2025-11-23 19:02:39 +0530

    Assign-4.0

---
+++ Assign-4.py
@@ -0,0 +1,23 @@
+#Q1
+x=11
+print("Value of  x =",x)
+
+#Q2
+n="Bablu Singh"
+print("My name is ",n)
+
+#Q3
+a,b,c=10,20,30
+print(a,b,c,sep='\n')
+
+#Q4
+a,b,c,d,e=35,True,4.5,3+4j,'MySirG'
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
+
+#Q5
+d,m,y=23,11,2025
+print(d,m,y,sep='/')
