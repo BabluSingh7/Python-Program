48aa05c622ece2b5d3764bfd085f5cb9af0ecc71
BabluSingh7 <bablusinghkb1@gmail.com>
2025-11-29 20:24:41 +0530

    Assign-7.0

---
+++ Assign-7.py
@@ -0,0 +1,23 @@
+#Q-1
+'''
+print("Enter any number print binary:")
+n = int(input())
+print(bin(n))
+
+#Q-2
+b =0b1100101
+print(int(b))
+'''
+#Q-3
+h =0x2f
+print(oct(h))
+
+#Q-4
+o =0o125
+print(bin(o))
+
+#Q-5
+o = 0o25
+h = 0x39
+b = o+h
+print(bin(b))
