c0e6d0e8d375bd197b9fa2d168021a10276f4637
BabluSingh7 <bablusinghkb1@gmail.com>
2025-11-29 20:39:33 +0530

    Assign-8.0

---
+++ Assign-8.py
@@ -0,0 +1,20 @@
+#Q-1
+n=input("Enter your name:")
+print("your Name is ",n)
+
+#Q-2 Enter two number and sum
+a,b = int(input("Enter two number")),int(input())
+print("Sum is ",a+b)
+
+#Q-3 Area of circle
+r = int(input("Enter a radius:"))
+print("Area of circle is = ",3.14*r**2)
+
+#Q-4 Square print
+n = int(input("Enter a number"))
+print("Square is ",n**2)
+
+#Q-5 takes a charecter print unicode
+c = input("Enter a charecter:")
+print(ord(c))
+
