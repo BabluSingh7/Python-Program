91a9c9acfa7eb69eeb850f5bbfafc979f84187b1
BabluSingh7 <bablusinghkb1@gmail.com>
2025-12-15 09:56:46 +0530

    Assign-9.0

---
+++ Assign-9.py
@@ -0,0 +1,8 @@
+#Q-1
+'''
+p,r,t=int(input("Enter Amout Rate Time")),int(input()),int(input())
+si=(p*r*t)/100
+print("SI is = ",si)
+'''
+#Q-2
+l,w=int(input("Enter a length and breadth :")),int(input())
