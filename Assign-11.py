4a4dda6b5cc6cec00b02d17e627df62f200dc42e
BabluSingh7 <bablusinghkb1@gmail.com>
2026-01-01 18:37:39 +0530

    Assign-11.0

---
+++ Assign-11.py
@@ -0,0 +1,17 @@
+#Q-1 cheek if string is my 
+s = input("Enter a string:")
+print('my' in s)
+
+#Q-2
+s1 = input("Enter two string ")
+s2 = input()
+print(s1 is s2)
+
+#Q-3
+print(5>10<5)
+
+#Q-4
+print("Red" and "White")
+
+#Q-5
+print(True or False)
