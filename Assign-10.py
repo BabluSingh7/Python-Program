e47f1bd81b7108e064623a6f061231da4e2a36f6
BabluSingh7 <bablusinghkb1@gmail.com>
2026-01-01 18:19:29 +0530

    Assign-10

---
+++ Assign-10.py
@@ -0,0 +1,27 @@
+#Q-1  Remove list digit 
+'''
+n = int(input("Enter a number"))
+l = n//10
+print("without lastdigit is = ",l)
+
+#Q-2  print last digit
+a = int(input("Enter a number:"))
+print("print las digit is = ",a%10)
+
+#Q-3 swap two number
+x,y=int(input("Enter two number:")),int(input())
+z = x
+x = y
+y = z
+print("Swap two number :",x,y)
+
+#Q-4 three digit number and print first digit
+num = int(input("Enter three digit number:"))
+num = num//100
+print("print first digit = ",num)
+'''
+#Q-5 three digit number print middle digit
+nu = int(input("Enter three digit number :"))
+nu = nu//10
+nu = nu%10
+print("print middle digit is = ",nu)
