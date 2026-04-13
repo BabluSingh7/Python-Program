a05b4dc3604bcea67b45ff5fd61730c5e93a4bab
BabluSingh7 <bablusinghkb1@gmail.com>
2026-04-13 17:53:21 +0530

    first V1.0.0

---
+++ Assign-5.py
@@ -0,0 +1,31 @@
+#Q1
+import keyword
+print(keyword.kwlist)
+
+#Q2
+# pyhon shall
+#>>> help()
+
+#Q3
+'''
+A0.py
+x=10
+
+A1.py
+import A0
+print(A0.x)
+
+'''
+#Q4
+x = True
+y = False
+z = None
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+#Q5
+a = 10
+print(a)
+del a
+print(a)
