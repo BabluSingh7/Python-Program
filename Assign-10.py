9ddece1de9fbdf524367e92653ba18c2c0dec68d
BabluSingh7 <bablusinghkb1@gmail.com>
2026-04-23 15:18:29 +0530

    first V1.0.0

---
+++ Assign-10.py
@@ -0,0 +1,26 @@
+#Q1
+n=2345
+print("print the without last digit",n//10)
+
+#Q2
+n=2348
+print("print the last digit",n%10)
+
+#Q3
+a,b=int(input("Enter the two numbers: ")),int(input())
+c=a
+a=b
+b=c
+print("After swapping:")
+print("swapped numbers:", a,b)
+
+#Q4
+n= int(input("Enter three digit number: "))
+print("print the first digit",n//100)
+
+
+#Q5
+n= int(input("Enter three digit number: "))
+print("print the middle digit",n//10%10)
+
+
