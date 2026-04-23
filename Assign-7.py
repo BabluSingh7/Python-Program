37605b4589e10f9665beb77714af5952deb8bfe0
BabluSingh7 <bablusinghkb1@gmail.com>
2026-04-23 14:26:44 +0530

    first V1.0.0

---
+++ Assign-7.py
@@ -0,0 +1,22 @@
+#Q1
+n=10
+print("binary of",n,"is",bin(n))
+
+#Q2
+b = 0b11001
+print("decimal of",b,"is",int(b))
+
+#Q3
+
+h =0x2F
+print("octal of",h,"is",oct(h))
+
+#Q4
+o =0o125
+print("binary of",o,"is",bin(o))
+
+#Q5
+o =0o25
+h =0x25
+s =o+h
+print("binary of",s,"is",bin(s))
