5ab1070b0e6bee1e2c94d6414b5b811999f26fd9
BabluSingh7 <bablusinghkb1@gmail.com>
2026-04-23 14:14:41 +0530

    first V1.0.0

---
+++ Assign-6.py
@@ -0,0 +1,24 @@
+#Q1
+
+num = 123
+print(str(num))
+
+#Q2
+
+c ='m'
+print(ord(c))
+
+#Q3
+
+u =100
+print("The character for ASCII value {} is = {}".format(u, chr(u)))
+
+#Q4
+s ='Hello, World!'
+#print(int(s)) Error
+
+#Q5
+i = 12
+print(bool(i))
+
+
