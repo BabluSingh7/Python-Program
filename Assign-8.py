a0a3229e15b58e9c5f24fc7d05f1c8880988a831
BabluSingh7 <bablusinghkb1@gmail.com>
2026-04-23 14:43:58 +0530

    first V1.0.0

---
+++ Assign-8.py
@@ -0,0 +1,24 @@
+#Q1
+name = input("Enter your name:")
+print("Hello:",name)
+
+#Q2
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+sum = num1 + num2
+print("Sum of",num1,"and",num2,"is=h",sum)
+
+#Q3
+
+radius = float(input("Enter radius of circle:"))
+area = 3.14 * radius ** 2
+print("Area of circle with radius",radius,"is",area)
+
+#Q4
+num = int(input("Enter a number:"))
+square = num ** 2
+print("Square of",num,"is",square)
+
+#Q5
+c = input("enter a character:")
+print("ASCII value of",c,"is",ord(c))
