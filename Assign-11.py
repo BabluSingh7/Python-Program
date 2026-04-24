3c30e6b6fdf9c5c45c8fbf2750f45316392863e0
BabluSingh7 <bablusinghkb1@gmail.com>
2026-04-24 14:42:53 +0530

    First V!.0.0

---
+++ Assign-11.py
@@ -0,0 +1,25 @@
+#Q1
+
+s=input("Enter a string :")
+if s=='my':
+    print("True")
+else:
+    print("false")
+
+#Q2
+s1,s2=input("Enter two string : "),input()
+if s1==s2:
+    print("True")
+else:
+    print("False")
+
+
+#Q3
+x = 5>10<5
+print(x)
+
+#Q4
+print("Red"and"White")
+
+#Q5
+print(True or False)
